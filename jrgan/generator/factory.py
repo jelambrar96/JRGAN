c25b8235988dea077d2ac4222a390e7d49f5fbd2
jelambrar <jelambrar@gmail.com>
2019-09-19 00:21:48 -0500

    fixed generator

--- jrgan/generator/factory.py
+++ jrgan/generator/factory.py
@@ -10,12 +10,12 @@ _list_gen = {
 
 class FactoryGen:
 
-    def getgen(type_gen, intput_shape, filters, model=False):
+    def getgen(type_gen, input_shape, filters, model=False):
         if isinstance(type_gen, int):
             type_gen = _list_gen[type_gen]
         gen = None
         if type_gen.upper() in ['SRGEN00', '00', '0']:
-            gen = SRGen00(intput_shape=intput_shape, filters=filters)
+            gen = SRGen00(input_shape=input_shape, filters=filters)
 
         if model:
             return gen.get_model()
--- jrgan/generator/srgen00.py
+++ jrgan/generator/srgen00.py
@@ -3,7 +3,9 @@ from keras.layers import BatchNormalization
 from keras.layers import Activation
 from keras.layers import Add
 from keras.layers import Input
-from keras.layers.convolutional import UpSampling2D, Conv2D
+# from keras.layers.convolutional import UpSampling2D, Conv2D
+from keras.layers import Conv2D
+from keras.layers import UpSampling2D
 from keras.models import Model
 
 from .basicgen import BasicGen
@@ -11,7 +13,11 @@ from .basicgen import BasicGen
 
 class SRGen00(BasicGen):
 
-    def __init__(self, input_shape, filters=32):
+    def __init__(self, input_shape, filters=32, n_blocks=16):
+        self._input_shape = input_shape
+        self._gf = filters
+        self._channels = self._input_shape[2]
+        self.n_residual_blocks = n_blocks
         # Low resolution image input
         img_lr = Input(shape=self._input_shape)
         # Pre-residual block
@@ -29,11 +35,12 @@ class SRGen00(BasicGen):
         u1 = self._deconv2d(c2)
         u2 = self._deconv2d(u1)
         # Generate high resolution output
-        gen_hr = Conv2D(self.channels, kernel_size=9, strides=1, padding='same', activation='tanh')(u2)
+        gen_hr = Conv2D(self._channels, kernel_size=9, strides=1,
+                        padding='same', activation='tanh')(u2)
         self._model = Model(img_lr, gen_hr)
 
     # create an residual
-    def _residual_block(layer_input, filters):
+    def _residual_block(self, layer_input, filters):
         # """Residual block described in paper"""
         d = Conv2D(filters, kernel_size=3, strides=1, padding='same')(layer_input)
         d = Activation('relu')(d)
@@ -44,10 +51,24 @@ class SRGen00(BasicGen):
         return d
 
     # create an deconv
-    def _deconv2d(layer_input):
+    def _deconv2d(self, layer_input):
         # """Layers used during upsampling"""
         u = UpSampling2D(size=2)(layer_input)
         u = Conv2D(256, kernel_size=3, strides=1, padding='same')(u)
         u = Activation('relu')(u)
         return u
+    """
+    def _add_residual_block(self, filters):
+        # self._model.add()
+        # conv2d =
+        self._model.add(Conv2D(filters=filters, kernel_size=(3, 3), strides=1,
+                        padding='same', activation='relu'))
+        self._model.add(BatchNormalization(momentum=0.8))
+        self._model.add(Conv2D(filters=filters, kernel_size=(3, 3), strides=1,
+                        padding='same'))
+        self._model.add(BatchNormalization(momentum=0.8))
 
+    def _add_deconv2d(self):
+        self._model.add(UpSampling2D(size=2))
+        self._model.add(Conv2D(256, kernel_size=3, strides=1, padding='same'))
+    """
